bd2e9b8b9b2fce3d7a283ad590f0d1a1f3d678fb
Gautier Jenkner <g.jenkner@gmail.com>
2018-05-22 23:39:38 +0200

    kernel

--- Symfony/app/AppKernel.php
+++ Symfony/app/AppKernel.php
@@ -37,7 +37,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
             new EddieJaoude\Bundle\SymfonyTranslationTwigCollectionBundle\EddieJaoudeSymfonyTranslationTwigCollectionBundle(),
-            new Bmatzner\FontAwesomeBundle\BmatznerFontAwesomeBundle(),
+            new SunCat\MobileDetectBundle\MobileDetectBundle(),
 
             // IMH specific
             new Imh\BaseBundle\ImhBaseBundle(),
